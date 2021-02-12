10a7d22b7a6a22b432eda8d5d18f2e2a2f96793e
LONFO FOFE <b.fofe@yahoo.com>
2021-02-12 11:31:23 +0100

    stateless& statefull

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,60 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State {
+  String _sentence;
+  bool _changed;
+
+  @override
+  void initState() {
+    _sentence = 'Salut les codeurs';
+    _changed = false;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Sygalin demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      title: 'Hello App',
       home: Scaffold(
-        body: Center(
-          child: Text(
-            "hello Sygalin",
-            style: TextStyle(
-              fontSize: 40,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                _sentence,
+                style: TextStyle(
+                  color: _changed ? Colors.purple : Colors.green,
+                  fontSize: 20.0,
+                ),
+              ),
             ),
-          ),
+            SizedBox(
+              height: 10.0,
+            ),
+            RaisedButton(
+              color: _changed ? Colors.white : Colors.black,
+              onPressed: () {
+                setState(() {
+                  _changed ? _changed = false : _changed = true;
+                  _sentence == 'Salut les codeurs'
+                      ? _sentence = 'Bye les codeurs'
+                      : _sentence = 'Salut les codeurs';
+                });
+              },
+              child: Text(
+                'Changer',
+                style: TextStyle(
+                  color: _changed ? Colors.black : Colors.white,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
