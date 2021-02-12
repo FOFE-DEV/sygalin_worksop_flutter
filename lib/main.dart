d4e4a06fa710fdad932e85b8746f3b5686083bf0
LONFO FOFE <b.fofe@yahoo.com>
2021-02-12 17:19:03 +0100

    make form

--- lib/main.dart
+++ lib/main.dart
@@ -10,62 +10,85 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State {
-  String _sentence;
-  bool _changed;
+  final _formKey = GlobalKey<FormState>();
+  final GlobalKey scaffoldKey = GlobalKey();
 
-  @override
-  void initState() {
-    _sentence = 'Salut les codeurs';
-    _changed = false;
-    super.initState();
-  }
-
+  String _name;
+  String _email;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Hello App',
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: Center(
-                child: Text(
-                  _sentence,
-                  style: TextStyle(
-                    color: _changed ? Colors.white : Colors.black,
-                    fontSize: 20.0,
+        body: Container(
+          margin: EdgeInsets.only(top: 25, left: 8, right: 8),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 50, left: 20, right: 20),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'name',
+                      hintText: 'enter name',
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Please enter some text";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      _name = value;
+                    },
                   ),
                 ),
-              ),
-              height: 200,
-              decoration: BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-              color: _changed ? Colors.white : Colors.black,
-              onPressed: () {
-                setState(() {
-                  _changed ? _changed = false : _changed = true;
-                  _sentence == 'Salut les codeurs'
-                      ? _sentence = 'Bye les codeurs'
-                      : _sentence = 'Salut les codeurs';
-                });
-              },
-              child: Text(
-                'Changer',
-                style: TextStyle(
-                  color: _changed ? Colors.black : Colors.white,
+                Container(
+                  margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'email',
+                      hintText: 'enter email',
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Please enter some text";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      _email = value;
+                    },
+                  ),
                 ),
-              ),
-            )
-          ],
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    print("hello");
+                    if (_formKey.currentState.validate()) {
+                      print(_email);
+                    }
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: Text(
+                      "Enregister",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
